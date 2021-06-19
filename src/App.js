84803bdc0075f8d81b6856b5a8457507a17543ba
Anubhab Karki <bhubhik@gmail.com>
2021-06-19 11:05:22 +1000

    Base Implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const apiKey = '37039385b60f47fdaff90331211806';
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+  const [weatherData, setWeatherData] = useState();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((loc) => {
+      setLat(loc.coords.latitude);
+      setLon(loc.coords.longitude);
+      console.log(lat, lon);
+    });
+    if (lat !== 0 && lon !== 0) {
+      const fetchData = async () => {
+        const response = await axios(
+          ` http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}`
+        );
+        console.log(response.data);
+        const results = response.data;
+        setWeatherData({
+          name: results.location.name,
+          country: results.location.country,
+          region: results.location.region,
+          temperature: results.current.temp_c,
+          description: results.current.condition.text,
+          icon: results.current.condition.icon,
+          humidity: results.current.humidity,
+        });
+        console.log(weatherData);
+      };
+      fetchData();
+    }
+  }, [lat, lon]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+      <h3>
+        Coordinates : latitude= {lat}, longitude= {lon}
+      </h3>
+      <ul>
+        <li>Name : {weatherData.name}</li>
+        <li>Country : {weatherData.country}</li>
+        <li>Temperature : {weatherData.temperature}</li>
+        <li>description : {weatherData.description}</li>
+      </ul>
+      <img src={weatherData.icon} alt='Icon' />
     </div>
   );
-}
+};
 
 export default App;
